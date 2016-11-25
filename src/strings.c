f5a6f264b1e5a96db9ebcf96de61833d07e7f341
Sandesh Ghimire <sandeshghimire@gmail.com>
2016-11-24 19:23:26 -0800

    String related problems

---
+++ src/strings.c
@@ -0,0 +1,296 @@
+/*
+ * Copyright 2016, Sandesh J. Ghimire
+ * All Rights Reserved.
+ */
+
+/** @file
+ * strings.c
+ * 
+ */
+
+/******************************************************
+ *                      Include Files
+ ******************************************************/
+
+#include <stdio.h>
+
+/******************************************************
+ *                      Macros
+ ******************************************************/
+
+/******************************************************
+ *                    Constants
+ ******************************************************/
+
+/******************************************************
+ *                   Enumerations
+ ******************************************************/
+
+/******************************************************
+ *                 Type Definitions
+ ******************************************************/
+
+/******************************************************
+ *                    Structures
+ ******************************************************/
+
+/******************************************************
+ *               Function Declarations
+ ******************************************************/
+
+char *strstr(const char *haystack, const char *needle);
+void myprintf(char *input, ...);
+void mystrcpy(char *dest, const char *src);
+int mystrcmp(const char *str1, const char *str2);
+int mysubstr(const char *str, char *token, const char *substr);
+int mycopy(void *src, void *dest, size_t count);
+int mytoupper(char * str);
+int mytolower(char * str);
+int mystrdup(char * src, char * dest);
+int mystrlen(char * str);
+
+
+/******************************************************
+ *       Test Suite  Function Declarations
+ ******************************************************/
+
+/******************************************************
+ *            Interrupt Handlers Mapping
+ ******************************************************/
+
+/******************************************************
+ *               Variables Definitions
+ ******************************************************/
+
+/******************************************************
+ *               Function Definitions
+ ******************************************************/
+
+/**
+ * Main entry point for clearBits 
+ */
+int main(int argc, char *argv)
+{
+
+    return 0;
+}
+
+/**
+ * locate a substring
+ *
+ * @param haystack[in]      : string 
+ * @param needle[in]        : substring 
+ 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return substring 
+ *
+ */
+char *strstr(const char *haystack, const char *needle)
+{
+
+}
+
+/**
+ * copyes memory content from one location to another 
+ *
+ * @param src[in]      : source of memory  
+ * @parm dest[in]      : destination memory location 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return errors  
+ *
+ */
+void myprintf(char *input, ...)
+{
+
+}
+
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+void mystrcpy(char *dest, const char *src)
+{
+
+}
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+int mystrcmp(const char *str1, const char *str2)
+{
+
+}
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+int mysubstr(const char *str, char *token, const char *substr)
+{
+
+}
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+int mycopy(void *src, void *dest, size_t count)
+{
+
+}
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+int mytoupper(char * str)
+{
+
+}
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+int mytolower(char * str)
+{
+
+}
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+int mystrdup(char * src, char * dest)
+{
+
+}
+/**
+ * convert char string into integer 
+ *
+ * @param str[in]      : char pointer 
+ * 
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ * @return integer 
+ *
+ */
+int mystrlen(char * str)
+{
+
+}
+
+/******************************************************
+ *               Test Suite 
+ ******************************************************/
+/**
+ * memcpy test 1
+ *
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ *
+ */
+void memcpy_test1()
+{
+}
+
+/**
+ * memcpy test 2
+ *
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ *
+ */
+void memcpy_test2()
+{
+}
+
+/**
+ * memmove test 1
+ *
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ *
+ */
+void memmove_test1()
+{
+}
+
+/**
+ * memmove test 2
+ *
+ *
+ * @note: This is not a generic function and does not support all architecture 
+ * @bugs: Has not been fully tested 
+ *
+ *
+ */
+void memmove_test2()
+{
+}
